543ffe2ffc0ed17483963ee18429823ca509dcfd
Anurag <anurag8731@gmail.com>
2026-02-08 10:45:35 +0530

    Day 2: Signup form with confirm password validation

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,64 @@ function App() {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
     console.log("Form Data", data);
-    alert("Login Successful");
+    alert("Signup Successful");
   };
 
+  const password = watch("password"); // Watch password field
+
   return (
-    <div style={{padding: 20}}>
-      <h1>Login Form</h1>
+    <div style={{ padding: 20 }}>
+      <h1>SignUp Form</h1>
 
       <form onSubmit={handleSubmit(onSubmit)}>
+        {/* USER NAME */}
+
+        <div>
+          <input
+            type="text"
+            placeholder="Enter User Name"
+            {...register("userName", {
+              required: "UserName is required",
+            })}
+          />
+
+          <p style={{ color: "red" }}>{errors.userName?.message}</p>
+        </div>
+
+        {/* NAME */}
+
+        <div>
+          <input
+            placeholder="Enter Name"
+            {...register("name", {
+              required: "Name is required",
+            })}
+          />
+
+          <p style={{ color: "red" }}>{errors.name?.message}</p>
+        </div>
 
         {/* EMAIL */}
 
         <div>
-          <input 
-          type="email"
-          placeholder="Enter Email"
-          {...register("email",{
-            required: "Email is required",
-            pattern: {
-              value: /^\S+@\S+$/i,
-              message: "Invalid email format",
-            },
-          })
-          }
+          <input
+            type="email"
+            placeholder="Enter Email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: "Invalid email format",
+              },
+            })}
           />
-          <p style={{color: "red"}}>
-            {errors.email?.message}
-          </p>
+          <p style={{ color: "red" }}>{errors.email?.message}</p>
         </div>
 
         {/* PASSWORD */}
@@ -51,15 +77,25 @@ function App() {
               },
             })}
           />
-          <p style={{ color: "red" }}>
-            {errors.password?.message}
-          </p>
+          <p style={{ color: "red" }}>{errors.password?.message}</p>
         </div>
 
-        <button type="submit">
-          Login
-        </button>
+        {/* CONFIRM PASSWORD */}
+        <div>
+          <input
+            type="passowrd"
+            placeholder="Confirm Password"
+            {...register("confirmPassword", {
+              required: "Confirm Password is required",
+              validate: (value) =>
+                value === password || "Password do not match",
+            })}
+          />
 
+          <p style={{ color: "red" }}>{errors.confirmPassword?.message}</p>
+        </div>
+
+        <button type="submit">Signup</button>
       </form>
     </div>
   );
